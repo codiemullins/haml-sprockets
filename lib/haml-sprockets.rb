ebc984595c893ae642e574a5b321c1e853da3cd1
TJ Peden <tj.peden@tjcoding.com>
2014-05-06 11:47:31 -0500

    Trying without the optimize function call

--- lib/haml-sprockets.rb
+++ lib/haml-sprockets.rb
@@ -23,7 +23,7 @@ module Haml
         haml_path = File.expand_path("../../vendor/assets/javascripts/haml.js", __FILE__)
         haml_lib = File.read(haml_path)
         context = ExecJS.compile(haml_lib)
-        return context.eval("Haml.optimize(Haml.compile('#{haml_code}', {escapeHtmlByDefault: true}))")
+        return context.eval("Haml.compile('#{haml_code}', {escapeHtmlByDefault: true})")
       end
     end
   end
